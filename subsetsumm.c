2512d2cd18e7d03173dd315e519f12489e0d4915
Allen N <61262169+allen3n@users.noreply.github.com>
2020-04-06 23:42:42 -0500

    subsetSum commit

---
+++ subsetsumm.c
@@ -0,0 +1,44 @@
+
+#include <stdio.h>
+#include <stdint.h>
+#define true 1
+#define false 0
+
+//k numbers that add up to v
+int subsetSum(int a[],int len, int k, int v) {
+	if (v == 0 && k == 0) {//we did it!
+		return true;
+	}
+	if (k <= 0) { //They all subtracted away the original too much its bad
+		return false;
+	}
+	//The two below will go through and will essentially check every subset of size k!
+	int find = v - a[0];
+	if (find >= 0) {
+		if (subsetSum(&a[1], len - 1, k - 1, find)) {
+			return true;
+		}
+		if (subsetSum(&a[1], len - 1, k, v)) {
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+	return false;
+}
+
+int main(void) {
+	int a[] = { 1,8,6,4,3 };
+	int len = 5;
+	int k = 2;
+	for (int v = 0; v != 22; v++) {
+		if (subsetSum(a, len, k, v)) {
+			printf("YES! There are %d numbers in 'a' that will add up to %d.\n", k, v);
+		}
+		else {
+			printf("NO! There are NO %d numbers in 'a' that will add up to %d.\n", k, v);
+		}
+	}
+
+}
